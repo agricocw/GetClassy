ff86106e1df65e9fc3e5c81571a5a07c1c791d7c
cwagr <agricocw@mail.uc.edu>
2024-01-17 01:22:36 -0500

    Progress

--- src/Person.java
+++ src/Person.java
@@ -24,4 +24,45 @@ public class Person {
         this.title = "";
         this.YOB = YOB;
     }
+
+    // Getters and Setters
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getYOB() {
+        return YOB;
+    }
+
+    public void setYOB(int YOB) {
+        this.YOB = YOB;
+    }
 }
